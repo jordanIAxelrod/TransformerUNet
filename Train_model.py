bfd965119ed875e85e4769c9e57dbdcb392fcd45
Jordan Axelrod <jordan.d.axelrod@gmail.com>
2022-08-03 18:26:54 -0400

    Dynamic loss chart, Main loop set up

--- Train_model.py
+++ Train_model.py
@@ -1,9 +1,28 @@
 import TransformerUNet
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import matplotlib.figure as figure
+
+import datetime as dt
+import numpy as np
 
 
 def train_loop(model: nn.Module, train_dataloader, optim, loss_eq):
+    """
+    Runs the training loop for the epoch
+    :param model: nn.Module
+    the model were training
+    :param train_dataloader: DataLoader
+    the training dataloader
+    :param optim: torch.optim
+    the optimizer were using
+    :param loss_eq: nn.Module
+    The loss of the model
+    :return: float
+    the average error of the epoch
+    """
     total_loss = 0
     model.train()
     for data in train_dataloader:
@@ -21,8 +40,19 @@ def train_loop(model: nn.Module, train_dataloader, optim, loss_eq):
 
     return total_loss / len(train_dataloader)
 
-def val_loop(model, val_dataloader, loss_eq):
 
+def val_loop(model: nn.Module, val_dataloader: DataLoader, loss_eq: nn.Module):
+    """
+    Runs the validation loop of training
+    :param model: nn.Module
+    the model were training
+    :param val_dataloader: DataLoader
+    the validation dataloader
+    :param loss_eq: nn.Module
+    The loss of the model
+    :return: float
+    the loss
+    """
     model.eval()
     total_loss = 0
     with torch.no_grad():
@@ -38,36 +68,105 @@ def val_loop(model, val_dataloader, loss_eq):
     return total_loss / len(val_dataloader)
 
 
-def fit(model, optim, loss_eq, train_dataloader, val_dataloader, epochs, url):
+def plot_loss(train_loss: float, val_loss: float, train_line: figure.Figure, val_line: figure.Figure):
+    """
+    Updates the plot of the losses
+    :param train_loss: float
+    loss of the training in this epoch
+    :param val_loss: float
+    loss of the validation in the epoch
+    :param train_line: figure.Figure
+    the line of the train loss
+    :param val_line: figure.Figure
+    the line of the validation loss
+    :return: None
+    """
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.ylim([-1, max([ymax, train_loss, val_loss]) + 2])
+    t_line = train_line.get_xdata()
+    if len(t_line) == 0:
+        x = 1
+    else:
+        x = t_line[-1] + 1
+    train_line.set_data(np.append(t_line, [x]), np.append(train_line.get_ydata(), [train_loss]))
 
+    val_line.set_data(np.append(val_line.get_xdata()), [x], np.append(val_line.get_ydata(), [val_loss]))
+
+    plt.draw()
+
+
+# TODO Early stopping algo
+def fit(
+        model: nn.Module,
+        optim: torch.optim,
+        loss_eq: nn.Module,
+        train_dataloader: DataLoader,
+        val_dataloader: DataLoader,
+        epochs: int,
+        url: str
+):
+    """
+    Fits the model for the specified amount of epochs and saves iterations of the model
+    :param model: nn.Module
+    The model to fit
+    :param optim: torch.optim
+    the optimizer of the model
+    :param loss_eq: nn.Module
+    the loss of the network
+    :param train_dataloader: DataLoader
+    dataloader of training data
+    :param val_dataloader: DataLoader
+    dataloader of the validation data
+    :param epochs: int
+    number of epochs to train for
+    :param url: str
+    the location to store the model
+    :return: tuple
+    the validation and training losses for each epoch
+    """
     train_loss, val_loss = [], []
-
+    train_line, = plt.plot([], [], color="black", label="Training loss")
+    val_line, = plt.plot([], [], color="red", label="Validation loss")
+    plt.legend()
+    plt.xlim([0, epochs])
     print("Training model")
     for epoch in range(epochs):
-        print("_"*25, f"Epoch {epoch + 1}", "_"*25)
+        try:
+            print("_" * 25, f"Epoch {epoch + 1}", "_" * 25)
 
-        train_loss.append(
-            train_loop(
-                model,
-                train_dataloader,
-                optim,
-                loss_eq
+            train_loss.append(
+                train_loop(
+                    model,
+                    train_dataloader,
+                    optim,
+                    loss_eq
+                )
             )
-        )
 
-        val_loss.append(
-            val_loop(
-                model,
-                val_dataloader,
-                loss_eq
+            val_loss.append(
+                val_loop(
+                    model,
+                    val_dataloader,
+                    loss_eq
+                )
             )
-        )
+            fmt = "%m.%d.%Y"
+            torch.save(
+                model,
+                url + f"{dt.datetime.now().strftime(fmt)} Transformer UNet {epoch}.pt"
+            )
+            print(f"Training loss: {train_loss[epoch]:.4f}")
+            print(f"Validation loss: {val_loss:.4f}")
+            plot_loss(train_loss[-1], val_loss[-1], train_line, val_line)
+            plt.savefig(url + f"{dt.datetime.now().srtftime(fmt)} Training and Validation Loss.png")
+        except KeyboardInterrupt as ki:
+            time = dt.datetime.now().strftime("%m.%d.%Y")
+            torch.save(
+                model,
+                url + f"{time} Transformer UNet Keyboard Interrupt {epoch}"
+            )
+            plt.savefig(url + f"{dt.datetime.now().srtftime(fmt)} Training and Validation Loss.png")
 
-        torch.save(
-            model,
-            url + f" {epoch}.pt"
-        )
-        print(f"Training loss: {train_loss[epoch]:.4f}")
-        print(f"Validation loss: {val_loss:.4f}")
+            raise ki
 
     return train_loss, val_loss
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import TransformerUNet
 import Train_model
-
+import torch.optim as optim
+import torch.nn.modules.loss as loss
 import CropVolumes
 
 import DataLoader
@@ -14,7 +15,41 @@ def clean():
 
 
 def fit_model():
-    pass
+    model = input(
+        "Please select the parameters of the model in the following order separated my commas enter nothing if using "
+        "default params: \nImage size, \nn_patches_down, \nn_patches_up,\nchan_list,\nmlp_ratio,\nqkv_bias,"
+        "\nn_layers,\ndropout, \nattn_dropout")
+    model = model.split(", ")
+    img_size, n_patches_down, n_patches_up, chan_list, mlp_ratio, qkv_bias,n_layers, p, attn_p = model
+    img_size = int(img_size)
+    n_patches_down = int(n_patches_down)
+    n_patches_up = int(n_patches_up)
+    chan_list = [int(chan) for chan in chan_list.split(", ")]
+    mlp_ratio = float(mlp_ratio)
+    qkv_bias = bool(qkv_bias)
+    n_layers = int(n_layers)
+    p = float(p)
+    attn_p = float(attn_p)
+    model = TransformerUNet.UNetViT(
+        img_size=img_size,
+        n_patches_down=n_patches_down,
+        n_patches_up=n_patches_up,
+        chan_list=chan_list,
+        mlp_ratio=mlp_ratio,
+        qkv_bias=qkv_bias,
+        n_layers=n_layers,
+        p=p,
+        attn_p=attn_p
+    )
+    opt = input("What optimizer shall be used")
+    if opt.lower() == "adam":
+        opt = optim.Adam(model.parameters(), lr=.0001)
+    else:
+        opt = optim.SGD(model.parameters(),lr=.0001)
+
+    loss_eq = loss.BCELoss()
+
+    Train_model.fit(model, opt, loss_eq, train_dataloader, val_dataloader, epochs)
 
 
 def main():
